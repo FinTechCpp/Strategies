d7f7dbfde476d3f23f81014203299b08c5884b68
maxime <maxime.deville@free.fr>
2026-01-02 10:20:12 +0100

    refactor: Remove distance comparison operators from FilterEvaluator and common.h

--- include/Managers/FilterEvaluator.hpp
+++ include/Managers/FilterEvaluator.hpp
@@ -154,16 +154,6 @@ private:
             case filter::ComparisonOperator::NOT_EQUAL:
                 return std::abs(left - right) >= 0.00001;
             
-            case filter::ComparisonOperator::DISTANCE_LESS: {
-                // absolute distance less than threshold
-                return std::abs(left - right) < threshold;
-            }
-
-            case filter::ComparisonOperator::DISTANCE_GREATER: {
-                // absolute distance greater than threshold
-                return std::abs(left - right) > threshold;
-            }
-            
             case filter::ComparisonOperator::CROSSES_ABOVE:
             case filter::ComparisonOperator::CROSSES_BELOW:
                 return false;
